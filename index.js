5a60aaa6ac1eb60e354953f2b44eca8209a73449
keatmin <keatmin@gmail.com>
2018-09-10 22:53:11 +0800

    add login

--- index.js
+++ index.js
@@ -14,8 +14,75 @@ server.use(bodyParser.urlencoded({
 server.use(bodyParser.json());
 
 server.post('/get-leave-balance', (req, res) => {
+	console.log('Request headers: ' + JSON.stringify(req.headers));
+	console.log('Request body: ' + JSON.stringify(req.body));
 
-    const staffToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.staffno ? req.body.queryResult		.parameters.staffno : 'Unknown';
+	const parameters = req.body.queryResult.parameters;
+	const outputContexts = req.body.queryResult.outputContexts;
+	console.log('parameters ==========>' +  JSON.stringify(parameters));
+	console.log('InputContexts ==========>' +  JSON.stringify(outputContexts));
+	
+	dispatchHandler(req,res)
+	
+});
+
+
+function dispatchHandler(req,res) {
+	let action = req.body.queryResult.intent.displayName;
+	console.log('action ==========>' +  action);
+	
+	if ( action == 'hr.leave.enquiry') {
+		getLeaveBalance(req,res);	
+	} else if ( action == 'hr.greetings.hello - login') {
+		login(req,res); 
+	}  	
+		
+}
+
+function login(req,res) {
+	
+	const userId = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.userid? req.body.queryResult.parameters.userid: 'Unknown';
+	console.log(userId);
+	
+	const passWord = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.password? req.body.queryResult.parameters.password: 'Unknown';
+	
+	console.log("UserId ===> " +  userId);
+	console.log("Password ===> " +  passWord);
+	
+	const reqUrl = encodeURI(`http://115.134.155.171:8080/web-student-tracker/rest/student/signOn/${userId}/${passWord}`);
+	console.log(reqUrl);
+    
+	http.get(reqUrl, (responseFromAPI) => {
+        let completeResponse = '';
+        responseFromAPI.on('data', (chunk) => {
+            completeResponse += chunk;
+        });
+        responseFromAPI.on('end', () => {
+            const token = completeResponse;
+			console.log("Token ===> " + token);
+            //let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
+            return res.json({
+                fulfillmentText: 'Login Success !!',
+                source: 'login',
+				outputContexts: [{"name":"login-authenticated", "lifespan":50, "parameters":{"token":token}}]
+				
+            });
+        });
+    }, (error) => {
+        return res.json({
+            fulfillmentText: 'Login Failed !!',
+            source: 'login'
+        });
+    });
+
+		
+	
+
+}
+	
+function getLeaveBalance(req,res) {
+
+	const staffToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.staffno ? req.body.queryResult.parameters.staffno : 'Unknown';
     console.log(staffToSearch);
 	const secret = 'secret';
 	const hash = crypto.createHmac('sha256', secret)
@@ -25,8 +92,6 @@ server.post('/get-leave-balance', (req, res) => {
 	const reqUrl = encodeURI(`http://115.134.155.171:8080/web-student-tracker/rest/student/${hash}/${staffToSearch}`);
 	console.log(reqUrl);
     
-	
-	
 	http.get(reqUrl, (responseFromAPI) => {
         let completeResponse = '';
         responseFromAPI.on('data', (chunk) => {
@@ -39,6 +104,7 @@ server.post('/get-leave-balance', (req, res) => {
             return res.json({
                 fulfillmentText: dataToSend,
                 source: 'get-leave-balance'
+				
             });
         });
     }, (error) => {
@@ -47,7 +113,10 @@ server.post('/get-leave-balance', (req, res) => {
             source: 'get-leave-balance'
         });
     });
-});
+
+	
+}	
+
 
 server.listen((process.env.PORT || 8000), () => {
     console.log("Server is up and running...");
